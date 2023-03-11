1e00c73456a30883edd929838bbaed78f4f24489
Djimi02 <mitko.manolev1@abv.bg>
2023-03-12 00:32:01 +0100

    Connection to firebase db added and simple login page

--- project/app/src/main/java/com/example/eventappproject/SignInActivity.java
+++ project/app/src/main/java/com/example/eventappproject/SignInActivity.java
@@ -1,14 +1,140 @@
 package com.example.eventappproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignInActivity extends AppCompatActivity {
+
+    private TextView goToRegistration;
+    private EditText emailTextLogin;
+    private EditText passwordTextLogin;
+    private Button btnLogin;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        initViews();
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    private void initViews() {
+        this.goToRegistration = findViewById(R.id.registerView);
+        this.btnLogin = findViewById(R.id.btnLogin);
+        this.emailTextLogin = findViewById(R.id.emailTextLogin);
+        this.passwordTextLogin = findViewById(R.id.passwordTextLogin);
+
+        btnLogin.setEnabled(false);
+
+        emailTextLogin.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (emailTextLogin.getText().toString().trim().length() > 0 && passwordTextLogin.getText().toString().trim().length() > 0) {
+                    btnLogin.setEnabled(true);
+                }
+            }
+        });
+
+        passwordTextLogin.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (passwordTextLogin.getText().toString().trim().length() > 0 || emailTextLogin.getText().toString().trim().length() > 0) {
+                    btnLogin.setEnabled(true);
+                }
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkData()){
+                    loginUser();
+                }
+            }
+        });
+
+        goToRegistration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                startActivity(new Intent(SignInActivity.this, RegisterUser.class));
+            }
+        });
+    }
+
+    private boolean checkData() {
+        String email = this.emailTextLogin.getText().toString().trim();
+        String password = this.passwordTextLogin.getText().toString().trim();
+
+        boolean output = true;
+
+        if (email.isEmpty()) {
+            this.emailTextLogin.setError("You should input your email!");
+            output = false;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            this.emailTextLogin.setError("You should input valid email!");
+            output = false;
+        }
+
+        if (password.isEmpty() || password.length() < 6) {
+            this.passwordTextLogin.setError("Password should have length of at least 6 characters!");
+            output = false;
+        }
+        return output;
+    }
+
+    private void loginUser() {
+        String email = this.emailTextLogin.getText().toString().trim();
+        String password = this.passwordTextLogin.getText().toString().trim();
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+//                    startActivity(new Intent(SignInActivity.this, UserProfile.class));
+                } else {
+                    Toast.makeText(SignInActivity.this, "Something went wrong! Please check your credentials!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
